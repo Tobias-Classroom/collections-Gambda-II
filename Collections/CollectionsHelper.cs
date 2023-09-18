c4dccda6c9e390f044b36ffa76ea1148237437c5
Gambda-II <alexanderlorengel_tgd@hotmail.de>
2023-09-18 14:33:59 +0200

    final version

--- Collections/CollectionsHelper.cs
+++ Collections/CollectionsHelper.cs
@@ -4,40 +4,50 @@ using System.Linq;
 
 namespace Collections
 {
-  public static class CollectionsHelper
-  {
-    public static List<int> AddRandomNumbersToList(List<int> numbers, int quantity)
+    public static class CollectionsHelper
     {
-      // your code here...
+        public static List<int> AddRandomNumbersToList(List<int> numbers, int quantity)
+        {
+            for (int k = 0; k < quantity; k++)
+            {
+                Random rnd = new Random();
+                numbers.Add(rnd.Next());
+            }
+
+            return numbers;
+        }
+
+        // output strings aus der Lösung gestibitzt
+        public static (List<int>, string) RemoveElementFromList(List<int> numbers, int element)
+        {
+            string text = "Element not found"; 
+            if (numbers.Contains(element))
+            {
+                numbers.Remove(element);
+                text = "Element removed successfully";
+            }
+
+            return (numbers, text);
+        }
+
+        public static bool CheckElementInList(List<int> numbers, int element)
+        {
+            return numbers.Contains(element);
+        }
+
+        public static int FindMax(List<int> numbers)
+        {
+            
+            return numbers.Count > 0 ? numbers.Max() : 0;
+        }
+        public static int FindMin(List<int> numbers)
+        {
+            return numbers.Count > 0 ? numbers.Min() : 0;
+        }
+        public static double CalculateAverage(List<int> numbers)
+        {
+            return numbers.Count > 0 ? numbers.Average() : 0;
+        }
 
     }
-
-    public static (List<int>, string) RemoveElementFromList(List<int> numbers, int element)
-    {
-      // your code here...
-
-    }
-
-    public static bool CheckElementInList(List<int> numbers, int element)
-    {
-      // your code here...
-
-    }
-
-    public static int FindMax(List<int> numbers)
-    {
-      // your code here...
-
-    }
-    public static int FindMin(List<int> numbers)
-    {
-      // your code here...
-
-    }
-    public static double CalculateAverage(List<int> numbers)
-    {
-      // your code here...
-    }
-
-  }
 }
